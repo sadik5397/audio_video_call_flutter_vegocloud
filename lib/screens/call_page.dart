8e57eec2da421134310306b37b54206460b7f300
codewithdarkwa <jnrdrkw1@gmail.com>
2022-11-01 16:32:38 +0000

    add app permission

--- lib/screens/call_page.dart
+++ lib/screens/call_page.dart
@@ -1,14 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:zego_uikit_prebuilt_call/zego_uikit_prebuilt_call.dart';
 
 class CallPage extends StatelessWidget {
-  const CallPage({Key? key}) : super(key: key);
-
+  const CallPage({Key? key, required this.callID}) : super(key: key);
+  final String callID;
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: SafeArea(
-        child: Text('Call Page'),
-      ),
+    return ZegoUIKitPrebuiltCall(
+      appID:
+          909621478, // Fill in the appID that you get from ZEGOCLOUD Admin Console.
+      appSign:
+          "111b58d37fa7d7903d411169d42a0d92671455cde73e341869b65f650174c669", // Fill in the appSign that you get from ZEGOCLOUD Admin Console.
+      userID: 'user_id',
+      userName: 'user_name',
+      callID: callID,
+      // You can also use groupVideo/groupVoice/oneOnOneVoice to make more types of calls.
+      config: ZegoUIKitPrebuiltCallConfig.oneOnOneVideoCall()
+        ..onOnlySelfInRoom = (context) => Navigator.of(context).pop(),
     );
   }
 }
