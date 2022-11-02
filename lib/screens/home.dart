3d0b2307f30355f75dc406c11ee400617989ae9b
codewithdarkwa <jnrdrkw1@gmail.com>
2022-11-02 16:16:06 +0000

    implement video call

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,13 +1,44 @@
 import 'package:flutter/material.dart';
 
+import 'call_page.dart';
+
 class Home extends StatelessWidget {
-  const Home({Key? key}) : super(key: key);
+  Home({Key? key}) : super(key: key);
+
+  final callIdController = TextEditingController(text: "call_id");
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: SafeArea(
-        child: Text('Zegocloud video call app'),
+    return Scaffold(
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 12.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Expanded(
+                child: TextFormField(
+                  controller: callIdController,
+                  decoration: const InputDecoration(
+                    labelText: "join a call by id",
+                  ),
+                ),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) {
+                      return CallPage(callID: callIdController.text);
+                    }),
+                  );
+                },
+                child: const Text('join'),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
